0c822bda0327eb41d4293e0c8961b47e772dcd08
protonaby <protonaby@gmail.com>
2019-11-29 14:39:55 +0200

    fix meeting

--- day5/hw-logical-tasks/meeting.js
+++ day5/hw-logical-tasks/meeting.js
@@ -18,11 +18,32 @@ function meeting(rooms, need) {
       }
     }
   });
+  if (need > 0) return 'Not enough!';
   return result;
 }
 
 //////////Testing///////////////////////////////////////////////////////////////
-console.log(meeting([['XXX', 3], ['XXXXX', 6], ['XXXXXX', 9]], 4)); //[0, 1, 3]
-console.log(meeting([['XXX', 3], ['XXXXX', 6], ['XXXXXX', 9], ['XXX',2]], 4)); //[0, 1, 3]
-console.log(meeting([['XXX', 1], ['XXXXXX', 6], ['X', 2], ['XXXXXX', 8], ['X', 3], ['XXX', 1]], 5) ); //[0, 0, 1, 2, 2]
-console.log(meeting([['XX', 2], ['XXXX', 6], ['XXXXX', 4]], 0));  //'Game On'
+console.log(meeting([
+  ['XXX', 3],
+  ['XXXXX', 6],
+  ['XXXXXX', 9]
+], 4)); //[0, 1, 3]
+console.log(meeting([
+  ['XXX', 3],
+  ['XXXXX', 6],
+  ['XXXXXX', 9],
+  ['XXX', 2]
+], 4)); //[0, 1, 3]
+console.log(meeting([
+  ['XXX', 1],
+  ['XXXXXX', 6],
+  ['X', 2],
+  ['XXXXXX', 8],
+  ['X', 3],
+  ['XXX', 1]
+], 5)); //[0, 0, 1, 2, 2]
+console.log(meeting([
+  ['XX', 2],
+  ['XXXX', 6],
+  ['XXXXX', 4]
+], 0)); //'Game On'
