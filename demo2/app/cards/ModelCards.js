3c98c3463ffcfa0e215caa7868b1044f065a432b
protonaby <protonaby@gmail.com>
2019-12-19 15:35:49 +0200

    [demo2] extend animals.json

--- demo2/app/cards/ModelCards.js
+++ demo2/app/cards/ModelCards.js
@@ -1,5 +1,7 @@
 export class ModelCards {
-
+    //todo add favicon
+    //todo emmet abbreviation
+    //todo count age from birth date
     constructor() {
         // this.urlBase = 'https://yevhverb.github.io/home-tasks/15/animals_en.json';
         // this.urlBase = 'https://maksv21.github.io/softserve/demo2/database/animals_en.json';
