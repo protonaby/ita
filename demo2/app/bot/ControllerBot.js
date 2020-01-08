8f36d8564268942538062a3c5468c9af5b184f92
protonaby <protonaby@gmail.com>
2020-01-08 12:44:39 +0200

    point bot to group chat

--- demo2/app/bot/ControllerBot.js
+++ demo2/app/bot/ControllerBot.js
@@ -3,8 +3,8 @@ import {TemplateBot} from './TemplateBot.js';
 export class ControllerBot {
 
     constructor({subscribe, notify}) {
-        this.chatId = -376016381;
-        // this.chatId = -377489566;
+        // this.chatId = -376016381;
+        this.chatId = -377489566;
         this.apiKey = '916202565:AAHUqbSIas5e8V2Tfu0CxUGooiUkUOGTTPU';
         this.subscribe = subscribe;
         this.subscribe('order-submitted', this.sendTlgMsg.bind(this));
